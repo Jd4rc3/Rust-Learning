9287778f2643935ad310aca8caf4b053f7eb16c1
Juan Daniel <juandanielarce398@gmail.com>
2023-03-03 18:17:39 -0500

    Defining a method in an enum
    Test

---
+++ Enums and Pattern Matching/src/CoinExplample.rs
@@ -0,0 +1,28 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
--- Enums and Pattern Matching/src/main.rs
+++ Enums and Pattern Matching/src/main.rs
@@ -1,13 +1,61 @@
 fn main() {
-    let home = IpAddrKind::V4(Ipv4Addr);
-    let loopback = IpAddrKind::V6(Ipv6Addr);
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("max is {}", max);
+    }
 }
 
-enum IpAddrKind {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChageColor(i32, i32, i32),
 }
 
-struct Ipv4Addr {}
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
 
-struct Ipv6Addr {}
+fn match_with_option() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn cat_all_and_placerholder() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
+    }
+
+    fn reroll() {}
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
+}
