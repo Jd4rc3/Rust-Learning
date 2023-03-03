8b1bd46f84911372dec618f21f9efeac3f2baf2d
Juan Daniel <juandanielarce398@gmail.com>
2023-03-03 18:08:24 -0500

    Binding a type to enum

--- Enums and Pattern Matching/src/main.rs
+++ Enums and Pattern Matching/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 }
 
 enum IpAddrKind {
-    V4,
-    V6
+    V4(String),
+    V6(String),
 }
